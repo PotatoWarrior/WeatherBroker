6e012b6072b9bb64921752d8bfda41d1eba83e00
Dmitriy <sailer123506@yandex.ru>
2016-12-08 16:54:38 +0400

    deploy

--- Listener/src/main/java/com/listener/MessageListener.java
+++ Listener/src/main/java/com/listener/MessageListener.java
@@ -5,12 +5,15 @@ import com.listener.dao.ForecastDao;
 import com.listener.exceptions.SaveException;
 import com.listener.exceptions.XmlParseException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsOperations;
 
 public class MessageListener {
     @Autowired
     private XmlToEntityConverter converter;
     @Autowired
     private ForecastDao dao;
+    @Autowired
+    private JmsOperations operations;
     public void onMessage(String xml){
         try {
             ForecastEntity entity = converter.convert(xml);
@@ -18,7 +21,7 @@ public class MessageListener {
         } catch (XmlParseException e) {
             e.printStackTrace();
         } catch (SaveException e){
-            //back to queue?
+            operations.convertAndSend(xml);
             e.printStackTrace();
         }
     }
